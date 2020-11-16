431314f5c4c9059c531faa41f11ae33e7ed5961a
Saied lakhdar loai <saied.lakhdar01@gmail.com>
2020-11-16 10:30:33 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const http = require('http');
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
-  response.sendStatus(200);// CODE BY KAHRBAA 
+  response.sendStatus(200);// CODE BY SoneOne  
 });
 app.listen(process.env.PORT);
-setInterval(() => { // CODE BY KAHRBAA 
+setInterval(() => { // CODE BY SoneOne  
   http.get(`http://op-quran.glitch.me/`);
 }, 280000);
 
@@ -13,30 +13,30 @@ setInterval(() => { // CODE BY KAHRBAA
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const bot = new Discord.Client();
-const ms = require("ms");// CODE BY KAHRBAA 
+const ms = require("ms");// CODE BY SoneOne  
 const fs = require('fs');
 const moment = require('moment');
 const request = require('request');
 const cmd = require("node-cmd");
-const prefix = "*"; // البرافيكس
-   const GUILDID = '745997515584831608'; // اي دي السيرفر  
-   const CHANNELID = '777794212133339146'; // اي دي الروم
+const prefix = "*"; // bot prefix
+   const GUILDID = '745997515584831608'; // server id 
+   const CHANNELID = '777794212133339146'; // room id 
 //======================================[Client]======================================
-// CODE BY KAHRBAA 
+// CODE BY SoneOne 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
    client.user.setActivity("One Piece Clan Playing Quran 24h",{type: 'WATCHING'})
 });
-// CODE BY KAHRBAA 
+// CODE BY SoneOne  
 const { Client } = require('discord.js');
-const ytdl  = require('ytdl-core');// CODE BY KAHRBAA 
-const url = 'https://www.youtube.com/watch?v=M6z0Qql4-qo'; // هنا فديو الخاص ب القرأن الكريم كامل // CODE BY KAHRBAA 
+const ytdl  = require('ytdl-core');// CODE BY SoneOne  
+const url = 'https://www.youtube.com/watch?v=M6z0Qql4-qo'; // quran url youtube vedio  // CODE BY SoneOne  
 
 
 client.on('ready',async () => {
     console.log('تـم تشغيل القرأن الكريم');
-    // CODE BY KAHRBAA 
+    // CODE BY SoneOne  
     voiceStay(GUILDID, CHANNELID);
    function voiceStay(guildid, channelid) {
     if(!guildid) throw new Error('ـاكد انك حطط ايدي السيرفر');
@@ -46,12 +46,12 @@ client.on('ready',async () => {
     const voiceChannel = guild.channels.get(channelid);;
     if (!voiceChannel) {
       return 
-    }// CODE BY KAHRBAA 
+    }// CODE BY SoneOne  
     voiceChannel.join()
       .then(connection => {
-        const stream = ytdl(url, { filter: 'audioonly' }); // CODE BY KAHRBAA 
+        const stream = ytdl(url, { filter: 'audioonly' }); // CODE BY SoneOne  
         const dispatcher = connection.playStream(stream);
-        dispatcher.on('end', () => { // CODE BY KAHRBAA  
+        dispatcher.on('end', () => { // CODE BY SoneOne   
           voiceChannel.leave();
           cmd.run("refresh")
 
@@ -71,29 +71,29 @@ client.on('message', message => {
     const now = new Date();
     const createdAt = millis / 1000 / 60 / 60 / 24;
     var heg = message.guild;
-// CODE BY KAHRBAA 
+// CODE BY SoneOne  
         const embed = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
-        .addField('GuidlOwner',message.guild.owner,true)// CODE BY KAHRBAA 
+        .addField('GuidlOwner',message.guild.owner,true)// CODE BY SoneOne  
         .addField('Guild ID', message.guild.id,true)
         .addField('Guild MemberCount', `${message.guild.memberCount}`+` [Online : ${message.guild.members.filter(m=>m.presence.status == 'online').size}]`)
         .addField('Guild Channels',`\`🔊\` ${message.guild.channels.filter(m => m.type === 'text').size} | `+`\`#\`${message.guild.channels.filter(m => m.type === 'voice').size} `)
         .addField('Guild RolesCount',` ${message.guild.roles.size} `,true)
         .addField('Created',`\`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
         .addField('Guild Region',message.guild.region,true)
-        // CODE BY KAHRBAA 
+        // CODE BY SoneOne  
         
         message.channel.send(embed)
-    }// CODE BY KAHRBAA 
+    }// CODE BY SoneOne  
 })
 
 client.on('message', message => {
-    if (message.content.startsWith(prefix + "stats")) {// CODE BY KAHRBAA 
+    if (message.content.startsWith(prefix + "stats")) {// CODE BY SoneOne  
                if(message.author.bot) return;
         if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
-    message.channel.send({// CODE BY KAHRBAA 
+    message.channel.send({// CODE BY SoneOne  
         embed: new Discord.RichEmbed()
-            .setColor('BLACK')// CODE BY KAHRBAA 
+            .setColor('BLACK')// CODE BY SoneOne  
             .addField('Ping' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
             .addField('RAM Usage', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
             .addField('ID' , `[ ${client.user.id} ]` , true)
@@ -102,7 +102,7 @@ client.on('message', message => {
     })
 }
 });
-      // CODE BY KAHRBAA 
+      // CODE BY SoneOne  
 
 
 
